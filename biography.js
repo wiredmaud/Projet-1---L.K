70551077446d191888430f6903d00e4e707185d8
Marine Grandcolas <marine-grandcolas_student2021@wilder.school>
2021-12-03 10:29:55 +0100

    Added HTML, CSS, JS and assets

---
+++ biography.js
@@ -0,0 +1,15 @@
+const clickLouise = document.getElementById('linkToLouise');
+const clickKarine = document.getElementById('linkToKarine');
+
+const displayLouise = document.getElementById('displayLouise');
+const displayKarine = document.getElementsByClassName('displayKarine');
+
+console.log(displayLouise);
+console.log(displayKarine);
+
+// clickLouise.addEventListener = ('click', function(event){
+//   event.preventDefault();
+//   if(displayLouise = true) {
+//     displayLouise.style.display = 'block';
+//     displayKarine.style.display = 'none';
+//   }})
