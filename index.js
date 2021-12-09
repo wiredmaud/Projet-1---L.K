e7c31a9588cf9eae952f4c7de971f6ccdfc9e9e9
Marine Grandcolas <marine-grandcolas_student2021@wilder.school>
2021-12-09 16:05:37 +0100

    added arrow for footer activation

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const clickLouise = document.querySelector('#buttonLouise');
 const clickKarine = document.querySelector('#buttonKarine');
 
-const displayLouise = document.querySelector('.displayLouiseNone');
-const displayKarine = document.querySelector('.displayKarineNone');
-const paragraphPresentation = document.querySelector('.presentation');
+const displayLouise = document.querySelector('#queryLouise');
+const displayKarine = document.querySelector('#queryKarine');
 /*All CLEAR with console!*/
-console.log(paragraphPresentation);
 
-clickLouise.addEventListener("click", function (event) {
+clickLouise.addEventListener('click', function (event) {
     event.preventDefault();
-    displayLouise.classList.toggle("displayLouiseFlex");
-    paragraphPresentation.classList.toggle("presentationDisplayNone");
+        if (displayLouise.classList.contains('heroPresentationNone')) {
+        displayLouise.classList.remove('heroPresentationNone');
+        displayLouise.classList.add('heroPresentationFlex');    
+    } else if (displayLouise.classList.contains('heroPresentationFlex')) {
+        displayLouise.classList.remove('heroPresentationFlex');
+        displayLouise.classList.add('heroPresentationNone');
+    }
+    if (displayKarine.classList.contains('heroPresentationFlex')) {
+        displayKarine.classList.remove('heroPresentationFlex');
+        displayKarine.classList.add('heroPresentationNone');
+    }
 });
 
-clickKarine.addEventListener("click", function (event) {
+clickKarine.addEventListener('click', function (event) {
     event.preventDefault();
-    displayKarine.classList.toggle('displayKarineFlex');
-    paragraphPresentation.classList.toggle("presentationDisplayNone");
+    if (displayKarine.classList.contains('heroPresentationNone')) {
+        displayKarine.classList.remove('heroPresentationNone');
+        displayKarine.classList.add('heroPresentationFlex');
+    } else if (displayKarine.classList.contains('heroPresentationFlex')) {
+        displayKarine.classList.remove('heroPresentationFlex');
+        displayKarine.classList.add('heroPresentationNone');
+    }
+    if (displayLouise.classList.contains('heroPresentationFlex')) {
+        displayLouise.classList.remove('heroPresentationFlex');
+        displayLouise.classList.add('heroPresentationNone');
+    }
 });
